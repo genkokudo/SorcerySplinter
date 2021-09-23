94dfb0b2b44d4dfdcae49bc64111b7104c24a311
ginpay <y-toyoda@live.jp>
2021-09-23 19:23:00 +0900

    取り敢えずデータグリッドを良い感じに表示

--- SorcerySplinter.Modules.Common/ViewModels/EditViewModel.cs
+++ SorcerySplinter.Modules.Common/ViewModels/EditViewModel.cs
@@ -5,21 +5,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SorcerySplinter.Modules.Common.ViewModels
 {
     public class EditViewModel : BindableBase
     {
-        private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
+        public List<SampleData> Variables { get; set; }
+
+        /// <summary>削除するコマンド</summary>
+        public DelegateCommand DeleteCommand { get; private set; }
 
         public EditViewModel()
         {
-            Message = "Edit from your Prism Module";
+            Variables = new List<SampleData>
+            {
+                new SampleData{SubName = "1郎", SubDescription = "説明1", SubDefValue = "aaaa", SubIsClassName = true },
+                new SampleData{SubName = "2郎", SubDescription = "説明2", SubDefValue = "bbbb", SubIsClassName = false },
+                new SampleData{SubName = "3郎", SubDescription = "説明3", SubDefValue = "cccc", SubIsClassName = true }
+            };
+            DeleteCommand = new DelegateCommand(DeleteRow);
         }
+
+        private void DeleteRow()
+        {
+            MessageBox.Show($"del");
+        }
+
+    }
+
+    public class SampleData
+    {
+        public string SubName { get; set; }
+        public string SubDescription { get; set; }
+        public string SubDefValue { get; set; }
+        public bool SubIsClassName { get; set; }
     }
 }
