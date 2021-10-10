50f782d06e2829d4ea476321448daa45421f6e97
ginpay <y-toyoda@live.jp>
2021-10-10 23:14:50 +0900

    スニペットの一覧を表示し、選択する

--- SorcerySplinter.Modules.Common/ViewModels/LoadViewModel.cs
+++ SorcerySplinter.Modules.Common/ViewModels/LoadViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Mvvm;
 using Prism.Regions;
 using SnippetGenerator;
+using SnippetGenerator.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +18,82 @@ namespace SorcerySplinter.Modules.Common.ViewModels
         /// <summary>スニペット出力サービス</summary>
         public ISnippetService SnippetService { get; set; }
 
+        /// <summary>言語選択時のコマンド</summary>
+        public DelegateCommand SelectLanguageCommand { get; private set; }
+
+        /// <summary>スニペット選択時のコマンド</summary>
+        public DelegateCommand SelectSnippetCommand { get; private set; }
+
+        /// <summary>画面アクセス時に読み込む全ての言語のスニペットリスト</summary>
+        public Dictionary<Language, List<SnippetInfo>> SnippetListDictionary;
+
+        // SnippetListDictionaryから選んだ言語のスニペット選択肢
+        private List<SnippetInfo> _snippetList;
+        public List<SnippetInfo> SnippetList
+        {
+            get { return _snippetList; }
+            set { SetProperty(ref _snippetList, value); }
+        }
+
+        // 言語選択肢
+        private Dictionary<string, Language> _languageDictionary;
+        public Dictionary<string, Language> LanguageDictionary
+        {
+            get { return _languageDictionary; }
+            set { SetProperty(ref _languageDictionary, value); }
+        }
+
+        // 選択した言語
+        private Language _language;
+        public Language Language
+        {
+            get { return _language; }
+            set { SetProperty(ref _language, value); }
+        }
+
+        // 選択したスニペット
+        private SnippetInfo _snippet;
+        public SnippetInfo Snippet
+        {
+            get { return _snippet; }
+            set { SetProperty(ref _snippet, value); }
+        }
+
         public LoadViewModel(ISnippetService snippetService)
         {
             SnippetService = snippetService;
+
+            // コマンド設定
+            SelectLanguageCommand = new DelegateCommand(SelectLanguage);
+            SelectSnippetCommand = new DelegateCommand(SelectSnippet);
+
+            // 初期値
+            Language = Language.CSharp;
+        }
+
+        /// <summary>
+        /// 言語選択時
+        /// スニペットのリストを表示する
+        /// 選択状態を解除する
+        /// </summary>
+        private void SelectLanguage()
+        {
+            //MessageBox.Show($"{Language}を選択", "だめじゃん", MessageBoxButton.OK, MessageBoxImage.Error);
+            // 読み込んでおいたスニペットリストから、ファイルリストを表示
+            SnippetList = SnippetListDictionary[Language];
+        }
+
+        /// <summary>
+        /// スニペット選択時
+        /// </summary>
+        private void SelectSnippet()
+        {
+            // TODO:ListViewは、選択すると「選択状態」にそのデータを置く
+            MessageBox.Show($"{Snippet.Discription}\n{Snippet.FullPath}", $"{Snippet.Title}を選択", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            // TODO:読み込みボタンを作成
+            // 「選択状態」のデータがあれば有効化。
+            // クリックしたら読み込んで編集画面に遷移する。
         }
 
 
@@ -51,9 +125,11 @@ namespace SorcerySplinter.Modules.Common.ViewModels
                 }
             }
 
-            // 読み込む
-            var list = SnippetService.GetSnippetList(SnippetDirectoryVs);
-            Console.WriteLine();
+            // 全ての言語のスニペットのリストを読み込む
+            SnippetListDictionary = SnippetService.GetSnippetList(SnippetDirectoryVs);
+
+            // 言語選択肢を作成する
+            LanguageDictionary = SnippetListDictionary.Keys.ToDictionary(t => t.ToString() == "CSharp" ? "C#" : t.ToString(), t => t);
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
